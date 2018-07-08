203b8e1fbc888dfbbdb14adfe56ba22efaaa3e61
jzhoulon <jzl_laozhou@163.com>
2018-07-08 14:56:46 +0800

    update count zero algo

--- test_algo/count_zero.cpp
+++ test_algo/count_zero.cpp
@@ -11,16 +11,20 @@ public:
 
   enum Algo {
     naive,
+    sort,
   };
   CountZeroPattern();
   ~CountZeroPattern();
   void naive_func();
+  void sort_func();
   void generateRandom();
   void setDataLen(int length);
+  void resetData();
   string getAlgoName(Algo a);
   void benchmark(Algo a);
   void printContent();
 private:
+  int* dataOrig_;
   int* data_;
   int len_;
 };
@@ -35,6 +39,11 @@ CountZeroPattern::~CountZeroPattern() {
 void CountZeroPattern::setDataLen(int length) {
   len_ = length;
   data_ =  new int[len_];
+  dataOrig_ = new int[len_];
+}
+
+void CountZeroPattern::resetData() {
+  memcpy(dataOrig_, data_, len_*sizeof(data_[0]));
 }
 
 void CountZeroPattern::generateRandom() {
@@ -44,26 +53,80 @@ void CountZeroPattern::generateRandom() {
   for (int i =0; i < len_; i++) {
     data_[i] = random();
   }
+  resetData();
 }
 void CountZeroPattern::naive_func() {
   int count = 0;
   for (int i = 0; i < len_; i++) {
     for (int j = i+1; j < len_; j++) {
-      if (data_[i] + data_[j] == 0) {
+      if (dataOrig_[i] + dataOrig_[j] == 0) {
         count++;
-        std::cout << "(" << i << ":" << data_[i] <<',' << j << ":" << data_[j] << ")," ;
       }
-          
     }
-  } 
+  }
+  std::cout << std::endl;
 
   std::cout<<std::endl<< "naive func = " << count << endl; 
+}
+
+
+void CountZeroPattern::sort_func() {
+  int count = 0;
+  int num_neg = 0;
+  int num_pos = 0;
+  int index_zero;
+  std::sort(data_, data_+len_);
+
+  int i = 0, j = len_ -1;
+  while(i < j) {
+    if (data_[i] > 0) break;
+    else if (data_[i] == 0) {index_zero = i; break;}
+    if (data_[j] <0) break;
+    else if(data_[j] == 0) {index_zero == j; break;}
+    if (data_[i] + data_[j] != 0) {
+      if (data_[i] + data_[j] > 0) j--;
+      else i++;
+    } else {
+      do {
+        i++;
+        num_neg++;
+      } while(data_[i] == data_[i-1]);
+      do {
+        j--;
+        num_pos++;
+      } while(data_[j] == data_[j+1]);
+      count+= num_neg*num_pos;
+      num_neg = 0; num_pos= 0;
+    }
+  }
+    int num_zero = 0;
+     if (data_[index_zero+1] == 0) {
+       while(data_[index_zero] == 0) {
+        num_zero++;
+        index_zero++;
+       }
+     } else if (data_[index_zero -1 ] == 0) {
+       while(data_[index_zero] == 0) {
+         num_zero++;
+         index_zero--;
+       }
+     } else {
+       num_zero++;
+     }
+
+    count += num_zero * (num_zero -1)/2;
+  std::cout << std::endl<< "sort func " << count << std::endl;
 } 
+
+
 string CountZeroPattern::getAlgoName(Algo a) {
   string name;
   switch (a) {
     case naive:
         name = "naive algo";
+        break;
+    case sort:
+        name = "sort algo";
         break;
     default:
         std::cout << "unsupported algo" << std::endl;
@@ -85,6 +148,8 @@ void CountZeroPattern::benchmark(Algo a) {
     case naive:
       naive_func();
       break;
+    case sort:
+      sort_func();
     default:
       std::cout << "unsupported algo" << std::endl;
   }
@@ -96,10 +161,12 @@ void CountZeroPattern::benchmark(Algo a) {
 
 int main(int argc, char **argv) {
   CountZeroPattern c;
-  c.setDataLen(200);
+  c.setDataLen(100000);
   c.generateRandom();
   c.benchmark(CountZeroPattern::Algo::naive);
-  c.printContent();
+ // c.printContent();
+  c.benchmark(CountZeroPattern::Algo::sort);
+ //c.printContent();
   return 0;
 }
 
